40d55e2ffdad85e3a61f9329b40a44badbba9b2b
livetau <livetau@paratools07.rrp.net>
2014-03-04 01:14:16 -0800

    pipeline commit 3

--- 2/3/pipe.c
+++ 2/3/pipe.c
@@ -47,10 +47,44 @@ if(PIPELINE == 1){
 }
 //divide and conquer method.
 else{
-	MPI_Bcast(data, 2, MPI_LONG_LONG_INT, 0, MPI_COMM_WORLD);
+	if(mid == 0){
+		MPI_Bcast(data, 2, MPI_LONG_LONG_INT, 0, MPI_COMM_WORLD);
+	}
 	
+	int mid = numprocs/2;
+	
+	if(myid < mid){
+		data[0] = data[0] * data[0];
+		MPI_Send(data, 2, MPI_LONG_LONG_INT, (myid+mid), 0, MPI_COMM_WORLD);
+	}
+	else{
+		MPI_Recv(data, 2, MPI_LONG_LONG_INT, myid-mid, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	}
 
-}	
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	int mid2 = (numprocs-mid)/2
+	if(myid >= mid && myid < mid+mid2){
+		data[0] = data[0] * data[0];
+		MPI_Send(data, 2, MPI_LONG_LONG_INT, (myid+mid2), 0, MPI_COMM_WORLD);
+	}
+	else if(myid >= mid){
+		MPI_Recv(data, 2, MPI_LONG_LONG_INT, myid-mid2, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	}	
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	int mid3 = (numprocs-mid-mid2)/2
+	if(myid >= mid + mid2 && myid < mid+mid2+mid3){
+		data[0] = data[0] * data[0];
+		MPI_Send(data, 2, MPI_LONG_LONG_INT, (myid+mid2+mid3), 0, MPI_COMM_WORLD);
+	}
+	else if(myid >= mid + mid2){
+		MPI_Recv(data, 2, MPI_LONG_LONG_INT, myid-mid2-mid3, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	}
+
+	if(myid == numprocs - 1){
+	 printf("The total is %d after %d iterations.\n", data[0]*data[0], POWER);	
+}	}
 MPI_Finalize();	
 return 0;
 }
